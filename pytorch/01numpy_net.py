f05aec380f77ff9ce6680d810de72035d6f10328
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-08-18 08:47:14 +0300

    reimplemented the numpy neural network

--- pytorch/01numpy_net.py
+++ pytorch/01numpy_net.py
@@ -1,26 +1,24 @@
-import torch
-
-dtype = torch.float
-device = torch.device("cpu")
-# device = torch.device("cuda:0") # Uncomment this to run on GPU
+# -*- coding: utf-8 -*-
+import numpy as np
+import math
 
 # Create random input and output data
-x = torch.linspace(-torch.pi, torch.pi, 2000, device=device, dtype=dtype)
-y = torch.sin(x)
+x = np.linspace(-math.pi, math.pi, 2000)
+y = np.sin(x)
 
 # Randomly initialize weights
-a = torch.randn((), device=device, dtype=dtype)
-b = torch.randn((), device=device, dtype=dtype)
-c = torch.randn((), device=device, dtype=dtype)
-d = torch.randn((), device=device, dtype=dtype)
+a = np.random.randn()
+b = np.random.randn()
+c = np.random.randn()
+d = np.random.randn()
 
 learning_rate = 1e-6
 for t in range(2000):
-    # Forward pass: compute predicted y
+    # Forward pass
     y_pred = a + b * x + c * x ** 2 + d * x ** 3
 
     # Compute and print loss
-    loss = (y_pred - y).pow(2).sum().item()
+    loss = np.square(y_pred - y).sum()
     if t % 100 == 99:
         print(t, loss)
 
@@ -31,11 +29,10 @@ for t in range(2000):
     grad_c = (grad_y_pred * x ** 2).sum()
     grad_d = (grad_y_pred * x ** 3).sum()
 
-    # Update weights using gradient descent
+    # Update weights
     a -= learning_rate * grad_a
     b -= learning_rate * grad_b
     c -= learning_rate * grad_c
     d -= learning_rate * grad_d
 
-
-print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+print(f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3')
